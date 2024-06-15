267c18cd8a98ac97b70adce5c0f142fb2690b708
Tim Rowe <tim@tjsr.id.au>
2024-06-15 19:34:20 +1000

    Add test templates.

---
+++ src/api/session.test.ts
@@ -0,0 +1,15 @@
+import { describe, test } from 'vitest';
+
+describe('session', () => {
+  test.todo('Should generate a new session ID if no existing one is given.', () => {
+  
+  });
+
+  test.todo('Should generate and return a new connect.sid when an existing one is provided.', () => {
+
+  });
+
+  test.todo('Should generate and return a new X-sesison-id when an existing one is provided.', () => {
+
+  });
+}); 
