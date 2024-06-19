77eb901bd8d6a7e1626e7307575fd1417efc4236
Tim Rowe <tim@tjsr.id.au>
2024-06-20 00:03:30 +1000

    Move sessionChecks to own file.

---
+++ src/session/sessionChecks.ts
@@ -0,0 +1,39 @@
+import {
+  RequestSessionIdRequiredError,
+  SessionDataNotFoundError,
+  SessionIDValueMismatch,
+  SessionIdRequiredError,
+  SessionIdTypeError
+} from '../errors/errorClasses.js';
+
+import { Session } from '../express-session/index.js';
+
+export const requireSessionIsIsString = (session: Session) => {
+  if (typeof session.id !== 'string') {
+    throw new SessionIdTypeError(
+      `Session ID defined on session is not a uuid (${typeof session.id}) when assigning userId.`);
+  }
+};export const requireSessionId = (session: Session) => {
+  if (!session.id) {
+    throw new SessionIdRequiredError('Session ID is not defined on session when assigning userId to session.');
+  }
+};
+export const requireRequestSessionId = (sessionID: string | undefined) => {
+  if (!sessionID) {
+    throw new RequestSessionIdRequiredError('Request sessionID is not defined when assigning userId to session.');
+  }
+};
+export const requireSessionInitialized = (session: Session) => {
+  if (!session) {
+    throw new SessionDataNotFoundError('Session is not defined when assigning userId to session.');
+  }
+};
+export const requireSessionIDValuesMatch = (
+  sessionID: string,
+  sessionIDFromSession: string
+) => {
+  if (sessionID !== sessionIDFromSession) {
+    throw new SessionIDValueMismatch(sessionID, sessionIDFromSession);
+  }
+};
+
