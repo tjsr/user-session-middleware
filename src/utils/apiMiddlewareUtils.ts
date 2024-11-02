8e8f5085698e99b4b649efdd63071af87febe690
Tim Rowe <tim@tjsr.id.au>
2024-11-02 15:54:15 +1100

    Fix missing namespace.

--- src/utils/apiMiddlewareUtils.ts
+++ src/utils/apiMiddlewareUtils.ts
@@ -10,12 +10,13 @@ import {
 import {
   UserSessionMiddlewareRequestHandler
 } from '../types/middlewareHandlerTypes.js';
+import { getAppUserIdNamespace } from '../auth/userNamespace.js';
 import { getUserIdFromSession } from '../auth/user.js';
 
 // import express as origExpressfrom 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const endWithJsonMessage = async <ResponseType extends express.Response<JSON|any, any>>(
+export const endWithJsonMessage = async <ResponseType extends express.Response<JSON | any, any>>(
   res: ResponseType,
   status: number | HttpStatusCode,
   message: string,
@@ -46,16 +47,19 @@ export const validateHasUserId: UserSessionMiddlewareRequestHandler = (
   next: express.NextFunction
 ): void => {
   try {
+    const userIdNamespace = getAppUserIdNamespace(request.app);
     // TODO: Fix casting here.
-    getUserIdFromSession(request.session).then((userId: UserId|undefined) => {
-      if (userId === undefined) {
-        return endWithJsonMessage(response, HttpStatusCode.UNAUTHORIZED, 'Invalid user', next);
-      }
-      console.debug(validateHasUserId, 'Got valid userId', userId);
-      return next();
-    }).catch((error: Error) => {
-      return next(error);
-    });
+    getUserIdFromSession(userIdNamespace, request.session)
+      .then((userId: UserId | undefined) => {
+        if (userId === undefined) {
+          return endWithJsonMessage(response, HttpStatusCode.UNAUTHORIZED, 'Invalid user', next);
+        }
+        console.debug(validateHasUserId, 'Got valid userId', userId);
+        return next();
+      })
+      .catch((error: Error) => {
+        return next(error);
+      });
   } catch (error) {
     console.warn('Got an exception when getting userId data', error);
     endWithJsonMessage(response, HttpStatusCode.INTERNAL_SERVER_ERROR, 'Invalid user', next);
