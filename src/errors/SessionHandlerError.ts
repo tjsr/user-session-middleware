40b3d55b3076c8d42937ac362f2068bc378a2dcb
Tim Rowe <tim@tjsr.id.au>
2024-11-16 22:32:32 +1100

    Add session deps, ensure app config setting for SID is set.

--- src/errors/SessionHandlerError.ts
+++ src/errors/SessionHandlerError.ts
@@ -1,58 +1,18 @@
-import { DEFAULT_ERROR_CODES } from "./defaultErrorCodes.js";
-import { SessionMiddlewareError } from "./SessionMiddlewareError.js";
-import { ErrorRequestHandler, Handler } from "../express/index.js";
+import { ErrorRequestHandler, Handler } from '../express/index.js';
+import { DEFAULT_ERROR_CODES } from './defaultErrorCodes.js';
+import { SessionMiddlewareError } from './SessionMiddlewareError.js';
 
 export class SessionHandlerError extends SessionMiddlewareError {
-  public get status(): number {
-    if (this._status) {
-      return this._status;
-    }
-    const defaultCode = DEFAULT_ERROR_CODES.get(this._sessionErrorCode)?.status || 500;
-    return defaultCode;
-  }
-
-  public override get message(): string {
-    if (super.message === undefined) {
-      return super.message;
-    }
-    const defaultMessage = DEFAULT_ERROR_CODES.get(this._sessionErrorCode)?.message || 'Unknown error';
-    return defaultMessage;
-  }
-
-  public set handlerChain(chain: (Handler|ErrorRequestHandler)[]) {
-    this._handlerChain = chain;
-  }
-
-  public get handlerChain(): (Handler|ErrorRequestHandler)[]|undefined {
-    return this._handlerChain;
-  }
-
-  public get clientMessage(): string {
-    if (this._clientMessage) {
-      return this._clientMessage;
-    }
-    return this.message;
-  }
-
-  public set clientMessage(message: string) {
-    this._clientMessage = message;
-  }
-
-  private _handlerChain?: (Handler|ErrorRequestHandler)[];
-  private readonly _status?: number;
-  private readonly _sessionErrorCode: number;
   private _clientMessage?: string;
 
+  private _handlerChain?: (Handler | ErrorRequestHandler)[];
+  private readonly _sessionErrorCode: number;
+  private readonly _status?: number;
   // TODO: Make this constructor protected.
-  constructor (
-    sesisonErrorCode: number,
-    status?: number,
-    message?: string,
-    cause?: unknown
-  ) {
+  constructor(sesisonErrorCode: number, status?: number, message?: string, cause?: unknown) {
     super(message);
-    this.name = (this.constructor.name != 'SessionHandlerError' ? (this.constructor.name + ':') : '') +
-     'SessionHandlerError';
+    this.name =
+      (this.constructor.name != 'SessionHandlerError' ? this.constructor.name + ':' : '') + 'SessionHandlerError';
     this._sessionErrorCode = sesisonErrorCode;
     if (status) {
       this._status = status;
@@ -63,7 +23,42 @@ export class SessionHandlerError extends SessionMiddlewareError {
     if (cause) {
       this.cause = cause;
     }
-  };
+  }
+
+  public get clientMessage(): string {
+    if (this._clientMessage) {
+      return this._clientMessage;
+    }
+    return this.message;
+  }
+
+  public get handlerChain(): (Handler | ErrorRequestHandler)[] | undefined {
+    return this._handlerChain;
+  }
+
+  public override get message(): string {
+    if (super.message === undefined) {
+      return super.message;
+    }
+    const defaultMessage = DEFAULT_ERROR_CODES.get(this._sessionErrorCode)?.message || 'Unknown error';
+    return defaultMessage;
+  }
+
+  public get status(): number {
+    if (this._status) {
+      return this._status;
+    }
+    const defaultCode = DEFAULT_ERROR_CODES.get(this._sessionErrorCode)?.status || 500;
+    return defaultCode;
+  }
+
+  public set clientMessage(message: string) {
+    this._clientMessage = message;
+  }
+
+  public set handlerChain(chain: (Handler | ErrorRequestHandler)[]) {
+    this._handlerChain = chain;
+  }
 
   static override isType(error: Error): boolean {
     return error.name?.endsWith('SessionHandlerError');
