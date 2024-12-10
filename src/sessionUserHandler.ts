02a28956d5d4dc9e085390241ef49cd59a0f3fd0
Tim Rowe <tim@tjsr.id.au>
2024-12-11 01:12:42 +1100

    Fix missing call to next when assigning userId.

--- src/sessionUserHandler.ts
+++ src/sessionUserHandler.ts
@@ -21,12 +21,14 @@ export const handleAssignUserIdToRequestSessionWhenNoExistingSessionData: UserSe
   addCalledHandler(response, handleAssignUserIdToRequestSessionWhenNoExistingSessionData);
 
   // Use as an assertion
-  if (!getAppUserIdNamespace(request.app, next)) {
+  try {
+    const namespace = getAppUserIdNamespace(request.app, next);
+  } catch (error: any) {
     console.error(
       handleAssignUserIdToRequestSessionWhenNoExistingSessionData,
       'No namespace found for userId; cannot assign userId to session.'
     );
-    return;
+    return next(error);
   }
 
   if (response.locals?.retrievedSessionData) {
